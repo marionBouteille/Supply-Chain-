c95b392102e4f9ce1342d79a6fdf8bc9d58f3e25
Unknown <marionbouteille83@gmail.com>
2019-05-14 16:18:45 +0200

    correction yaml

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ http.createServer(function (req, res) {
     res.writeHead(200, {
         'Content-Type':'text/plain'
     });
-    res.write('New NEW NEW NEW Hello world !!!')
+    res.write('New Hello world !!!')
     res.end();
     console.log("Server running on port 3000");
 }).listen(3000);
