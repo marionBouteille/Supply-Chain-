410917a8e7885b37217d6a0694d58ee087f166a5
Unknown <marionbouteille83@gmail.com>
2019-05-14 15:50:46 +0200

    test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ http.createServer(function (req, res) {
     res.writeHead(200, {
         'Content-Type':'text/plain'
     });
-    res.write('New Hello world !!!')
+    res.write('New NEW Hello world !!!')
     res.end();
     console.log("Server running on port 3000");
 }).listen(3000);
